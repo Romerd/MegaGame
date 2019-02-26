7a548cc99714eaae24210b4b0cc38bc59450656e
Romerd <slavon1990@gmail.com>
2019-02-27 00:06:36 +0300

    New features and fix of previous shity bugs
    Was added: Camera Controll, Physics of Player instead of RigidBody + added camera rotation (still in progress,  now workin at moving player with right camera rotation)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offSet;
+    public bool useOffsetValues;
+    public float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (!useOffsetValues)
+        {
+            offSet = target.position - transform.position;
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the x position of the mouse & rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0,horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        target.Rotate(-vertical, 0, 0);
+        
+            //Move the camera based on the current rotation of the rarget & the original offset
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = target.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation  * offSet);
+        //transform.position = target.position - offSet;
+        transform.LookAt(target);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,47 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    public Rigidbody theRB;
+    //public Rigidbody theRB;
+    public float jumpForce;
+    public CharacterController controller;
+    public float gravityScale;
+
+    private Vector3 moveDirection;
 
     // Start is called before the first frame update
     void Start()
     {
-        theRB = GetComponent<Rigidbody>();
+        //theRB = GetComponent<Rigidbody>();
+        controller = GetComponent <CharacterController>() ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        /*theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump")) //Проверяем на нажатие кнопки
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+        }
+        */
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (controller.isGrounded )
+        {
+            //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump")) //Проверяем на нажатие кнопки
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y  * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
