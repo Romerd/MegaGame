b42ed327facd5ea53fe824ac304e82dab8ea51a6
Romerd <slavon1990@gmail.com>
2019-03-10 19:44:00 +0300

    Fixed
    New animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,9 +10,9 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
     public float gravityScale;
     public float rotSpeed;
-    private float rot = 0f;
+    //private float rot = 0f;
 
-    public Animator anim;
+    private Animator anim;
 
 
     private Vector3 moveDirection = Vector3.zero;
@@ -28,77 +28,112 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+        GetInput();
+    }
 
+    void GetInput()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(anim.GetBool("running") == true)
+                {
+                    anim.SetBool("running", false);
+                    anim.SetInteger("condition", 0);
+                }
+
+                if (anim.GetBool("running") == false)
+                {
+                    Attacking();
+                }
+
+            }
+        }
+    }
+    void Movement()
+    {
         /*theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
 
-        if (Input.GetButtonDown("Jump")) //Проверяем на нажатие кнопки
-        {
-            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
-        }
-        */
+       if (Input.GetButtonDown("Jump")) //Проверяем на нажатие кнопки
+       {
+           theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+       }
+       */
 
         // moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
 
         float yStore = moveDirection.y;
-        moveDirection = (transform.forward * Input.GetAxis("Vertical")) 
+        moveDirection = (transform.forward * Input.GetAxis("Vertical"))
                             + (transform.right * Input.GetAxis("Horizontal"));
         moveDirection = moveDirection.normalized * moveSpeed;
         moveDirection.y = yStore;
 
-        if (controller.isGrounded )
+        if (controller.isGrounded)
         {
             //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
 
-            if (Input.GetKey (KeyCode.W))
+            if (Input.GetKey(KeyCode.W))
             {
-                anim.SetInteger("condition", 1);
-
-                if (Input.GetKey(KeyCode.Mouse0))
+                if (anim.GetBool("attacking") == true)
                 {
-                    anim.SetInteger("condition", 2);
+                    return;
+                }
+                else if (anim.GetBool("attacking") == false)
+                {
+                    anim.SetBool("running", true);
+                    anim.SetInteger("condition", 1);
                 }
 
-                if (Input.GetMouseButton(0))
-                {
-                    anim.SetInteger("condition", 4);
-                }
+
+                //if (Input.GetKey(KeyCode.Mouse0))
+                //{
+                //    anim.SetInteger("condition", 2);
+                //}
+
+                //if (Input.GetMouseButton(0))
+                //{
+                //    anim.SetInteger("condition", 4);
+                //}
                 //moveDirection = new Vector3(0, 0, 1);
                 //moveDirection *= moveSpeed;
                 //moveDirection = transform.TransformDirection(moveDirection);
             }
 
-            if (Input.GetKey (KeyCode.Mouse0))
-            {
-                anim.SetInteger("condition", 2);
-            }
+            //if (Input.GetKey (KeyCode.Mouse0))
+            //{
+            //    anim.SetInteger("condition", 2);
+            //}
 
-            if (Input.GetMouseButton(0))
-            {
-                anim.SetInteger("condition", 4);
-            }
+            //if (Input.GetMouseButton(0))
+            //{
+            //    anim.SetInteger("condition", 4);
+            //}
 
-            if (Input.GetKey (KeyCode.F))
+            if (Input.GetKey(KeyCode.F))
             {
                 anim.SetInteger("condition", 3);
             }
 
-            if (Input.GetKeyUp (KeyCode.F))
+            if (Input.GetKeyUp(KeyCode.F))
             {
                 anim.SetInteger("condition", 0);
             }
 
-            if (Input.GetMouseButtonUp(0))
-            {
-                anim.SetInteger("condition", 0);
-            }
+            //if (Input.GetMouseButtonUp(0))
+            //{
+            //    anim.SetInteger("condition", 0);
+            //}
 
-            if (Input.GetKeyUp (KeyCode.Mouse0))
-            {
-                anim.SetInteger("condition", 0);
-            }
+            //if (Input.GetKeyUp (KeyCode.Mouse0))
+            //{
+            //    anim.SetInteger("condition", 0);
+            //}
 
-            if (Input.GetKeyUp (KeyCode.W))
+            if (Input.GetKeyUp(KeyCode.W))
             {
+                anim.SetBool("running", false);
                 anim.SetInteger("condition", 0);
                 //moveDirection = new Vector3(0, 0, 0);
             }
@@ -118,5 +153,18 @@ public class PlayerController : MonoBehaviour
 
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);// * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
+    }
+    void Attacking()
+    {
+        StartCoroutine(AttackRoutine());
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        anim.SetBool("attacking", true);
+        anim.SetInteger("condition", 2);
+        yield return new WaitForSeconds(1);
+        anim.SetInteger("condition", 0);
+        anim.SetBool("attacking", false);
     }
 }
